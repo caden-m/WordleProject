890cb7bb16e6c7ef29491cc98573b94d112af0c4
BrittanyRobertson <90488960+BrittanyRobertson@users.noreply.github.com>
2022-10-01 21:26:52 -0600

    Milestone #2 code
    I added Milestone #2 code and Stefany's code as well for Milestone #1

--- Wordle.py
+++ Wordle.py
@@ -11,14 +11,30 @@ from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
+    word = random.choice(FIVE_LETTER_WORDS)
 
+    # I do not know what to call the input word and my mac won't run the program.. 
+    # so may have to test it on monday with everyone --Brittany
     def enter_action(s):
-        gw.show_message("You have to implement this method.")
+        __name__.lower()
+        if __name__ in FIVE_LETTER_WORDS:
+            gw.show_message("Great guess!")
+        else:
+            gw.show_message("Not in word list.")
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
+
+    c = 0 
+    for i in word:
+        gw.set_square_letter(0, c, i)
+        c+= 1
+        print(i,c)
 
 # Startup code
 
 if __name__ == "__main__":
     wordle()
+
+
+
