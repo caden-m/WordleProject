c7cf59323f9215535aeb5c672e95298536cd921b
matthew.mccord <matthew.mccord@vivint.com>
2022-10-05 12:37:46 -0600

    testing

--- Wordle.py
+++ Wordle.py
@@ -9,24 +9,39 @@ import enum
 import random
 
 
+
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import CORRECT_COLOR, MISSING_COLOR, PRESENT_COLOR, UNKNOWN_COLOR, WordleGWindow, N_COLS, N_ROWS
 
 # def wordle():
+#     word = random.choice(FIVE_LETTER_WORDS)
 
+#     # I do not know what to call the input word and my mac won't run the program.. 
+#     # so may have to test it on monday with everyone --Brittany
 #     def enter_action(s):
-#         gw.show_message("You have to implement this method.")
+#         __name__.lower()
+#         if __name__ in FIVE_LETTER_WORDS:
+#             gw.show_message("Great guess!")
+#         else:
+#             gw.show_message("Not in word list.")
 
 #     gw = WordleGWindow()
 #     gw.add_enter_listener(enter_action)
+
+#     c = 0 
+#     for i in word:
+#         gw.set_square_letter(0, c, i)
+#         c+= 1
+#         print(i,c)
 
 # # Startup code
 
 # if __name__ == "__main__":
 #     wordle()
-def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
-def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
-def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk))
+
+# def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
+# def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+# def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk))
 
 guess = 'tateo'
 Uguess = guess.lower()
@@ -43,19 +58,24 @@ row = 1
 col = 0
 color = UNKNOWN_COLOR
 
-for i in range(5):
-    if UcharacterizedGuess[i] == UcharacterizedActual[i]:
-        prGreen(UcharacterizedGuess[i])
-        def set_square_color(self, row, col, CORRECT_COLOR):
-            self._grid[row][col].set_color(CORRECT_COLOR)
-    elif UcharacterizedGuess[i] in UcharacterizedActual:
-        prYellow(UcharacterizedGuess[i])
-        def set_square_color(self, row, col, PRESENT_COLOR):
-            self._grid[1][i].set_color(PRESENT_COLOR)
+for i, guessLetter in enumerate(Uguess):
+    if Uguess[i] == Uactual[i]:
+        WordleGWindow.set_square_color(self, WordleGWindow.get_current_row(), i, CORRECT_COLOR)
+        Uactual = Uactual.replace(guessLetter, '-', 1)
+    elif guessLetter in Uactual:
+        WordleGWindow.set_square_color(self, WordleGWindow.get_current_row(), i, PRESENT_COLOR)
+        Uactual = Uactual.replace(guessLetter, '-', 1)
     else:
-        prLightGray(UcharacterizedGuess[i])
-        def set_square_color(self, row, col, MISSING_COLOR):
-            self._grid[1][i].set_color(MISSING_COLOR)
+        WordleGWindow.set_square_color(self, WordleGWindow.get_current_row(), i, MISSING_COLOR)
+    # if UcharacterizedGuess[i] == UcharacterizedActual[i]:
+    #     prGreen(UcharacterizedGuess[i])
+    #     WordleGWindow.set_square_color(WordleGWindow.get_current_row(), i, CORRECT_COLOR)
+    # elif UcharacterizedGuess[i] in UcharacterizedActual:
+    #     prYellow(UcharacterizedGuess[i])
+    #     WordleGWindow.set_square_color(WordleGWindow.get_current_row(), i, PRESENT_COLOR)
+    # else:
+    #     prLightGray(UcharacterizedGuess[i])
+    #     WordleGWindow.set_square_color(WordleGWindow.get_current_row(), i, MISSING_COLOR)
 
 # CORRECT_COLOR = "#66BB66"       # Light green for correct letters
 # PRESENT_COLOR = "#CCBB66"       # Brownish yellow for misplaced letters
