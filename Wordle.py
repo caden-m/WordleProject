5d9f84da93296b565c23a5a4af7e7ff02067a180
BrittanyRobertson <90488960+BrittanyRobertson@users.noreply.github.com>
2022-10-05 13:01:49 -0600

    fixed conflicts

--- Wordle.py
+++ Wordle.py
@@ -7,66 +7,27 @@ BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
 
 import enum
 import random
-
-
-
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import CORRECT_COLOR, MISSING_COLOR, PRESENT_COLOR, UNKNOWN_COLOR, WordleGWindow, N_COLS, N_ROWS
 
-<<<<<<< HEAD
 def wordle():
     word = random.choice(FIVE_LETTER_WORDS)
 
-    # I do not know what to call the input word and my mac won't run the program.. 
-    # so may have to test it on monday with everyone --Brittany
     def enter_action(s):
         __name__.lower()
         if __name__ in FIVE_LETTER_WORDS:
             gw.show_message("Great guess!")
         else:
             gw.show_message("Not in word list.")
-=======
-# def wordle():
-#     word = random.choice(FIVE_LETTER_WORDS)
 
-#     # I do not know what to call the input word and my mac won't run the program.. 
-#     # so may have to test it on monday with everyone --Brittany
-#     def enter_action(s):
-#         __name__.lower()
-#         if __name__ in FIVE_LETTER_WORDS:
-#             gw.show_message("Great guess!")
-#         else:
-#             gw.show_message("Not in word list.")
->>>>>>> MattColoring
-
-#     gw = WordleGWindow()
-#     gw.add_enter_listener(enter_action)
-
-<<<<<<< HEAD
     c = 0 
     for i in word:
         gw.set_square_letter(0, c, i)
         c+= 1
         print(i,c)
 
-# Startup code
-
 if __name__ == "__main__":
     wordle()
-
-
-
-=======
-#     c = 0 
-#     for i in word:
-#         gw.set_square_letter(0, c, i)
-#         c+= 1
-#         print(i,c)
-
-# # Startup code
-
-# if __name__ == "__main__":
-#     wordle()
 
 # def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
 # def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
@@ -112,4 +73,3 @@ for i, guessLetter in enumerate(Uguess):
 # MISSING_COLOR = "#999999"       # Gray for letters that don't appear
 # UNKNOWN_COLOR = "#FFFFFF"       # Undetermined letters are white
 # KEY_COLOR = "#DDDDDD"           # Keys are colored light gray
->>>>>>> MattColoring
