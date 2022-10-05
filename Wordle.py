777d5fd679eaf191bc0de8b452ec7f625a0392d5
BrittanyRobertson <90488960+BrittanyRobertson@users.noreply.github.com>
2022-10-05 12:29:20 -0600

    Enter work
    Played around with my function to try to get enter to work

--- Wordle.py
+++ Wordle.py
@@ -18,8 +18,14 @@ def wordle():
     # I do not know what to call the input word and my mac won't run the program.. 
     # so may have to test it on monday with everyone --Brittany
     def enter_action(s):
-        __name__.lower()
-        if __name__ in FIVE_LETTER_WORDS:
+        # g = 0 
+        # for i in s:
+        #     gw.set_square_letter(0, g, i)
+        #     c+= 1
+        #     print(i,g)
+
+        s.lower()
+        if s in FIVE_LETTER_WORDS:
             gw.show_message("Great guess!")
         else:
             gw.show_message("Not in word list.")
