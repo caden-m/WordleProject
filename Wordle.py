d27bbdf894c441147689b90df27c1180d84295b8
BrittanyRobertson <90488960+BrittanyRobertson@users.noreply.github.com>
2022-10-05 13:02:54 -0600

    updated conflicts

--- Wordle.py
+++ Wordle.py
@@ -14,17 +14,22 @@ def wordle():
     word = random.choice(FIVE_LETTER_WORDS)
 
     def enter_action(s):
-        __name__.lower()
-        if __name__ in FIVE_LETTER_WORDS:
+        s.lower()
+        if s in FIVE_LETTER_WORDS:
             gw.show_message("Great guess!")
         else:
             gw.show_message("Not in word list.")
+
+    gw = WordleGWindow()
+    gw.add_enter_listener(enter_action)
 
     c = 0 
     for i in word:
         gw.set_square_letter(0, c, i)
         c+= 1
         print(i,c)
+
+# Startup code
 
 if __name__ == "__main__":
     wordle()
@@ -73,3 +78,21 @@ for i, guessLetter in enumerate(Uguess):
 # MISSING_COLOR = "#999999"       # Gray for letters that don't appear
 # UNKNOWN_COLOR = "#FFFFFF"       # Undetermined letters are white
 # KEY_COLOR = "#DDDDDD"           # Keys are colored light gray
+# File: Wordle.py
+
+"""
+PM: Caden Marquis 
+SCRUM Master: Matthew McCord 
+SCRUM Team: Brittany Choi, Taylor Anderson, Stefany Guevara
+
+"""
+
+import random
+
+from WordleDictionary import FIVE_LETTER_WORDS
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+
+
+
+
+
