b175d3dfbc44cc2b9ca4d425c1ec30a1d658bc96
BrittanyRobertson <90488960+BrittanyRobertson@users.noreply.github.com>
2022-10-05 13:28:12 -0600

    changed variables

--- Wordle.py
+++ Wordle.py
@@ -4,7 +4,6 @@
 PM: Caden Marquis 
 SCRUM Master: Matthew McCord 
 SCRUM Team: Brittany Choi, Taylor Anderson, Stefany Guevara
-
 """
 
 import enum
@@ -16,30 +15,40 @@ def wordle():
     word = random.choice(FIVE_LETTER_WORDS)
 
     def enter_action(s):
+        row = 0
         s.lower()
         if s in FIVE_LETTER_WORDS:
             gw.show_message("Great guess!")
         else:
             gw.show_message("Not in word list.")
+
+        UcharacterizedGuess = list(s)
+        UcharacterizedActual = list(word)
+
+        print(UcharacterizedGuess)
+        print(UcharacterizedActual)
         
-        for i, guessLetter in enumerate(Uguess):
-            if Uguess[i] == Uactual[i]:
+        for i, guessLetter in enumerate(s):
+            if s[i] == word[i]:
                 WordleGWindow.set_square_color(gw, WordleGWindow.get_current_row(gw), i, CORRECT_COLOR)
-                Uactual = Uactual.replace(guessLetter, '-', 1)
-            elif guessLetter in Uactual:
+                word = word.replace(guessLetter, '-', 1)
+            elif guessLetter in word:
                 WordleGWindow.set_square_color(gw, WordleGWindow.get_current_row(gw), i, PRESENT_COLOR)
-                Uactual = Uactual.replace(guessLetter, '-', 1)
+                word = word.replace(guessLetter, '-', 1)
             else:
                 WordleGWindow.set_square_color(gw,WordleGWindow.get_current_row(gw), i, MISSING_COLOR)
 
+        row = row + 1
+
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
+    gw.set_current_row(row)
 
-    c = 0 
-    for i in word:
-        gw.set_square_letter(0, c, i)
-        c+= 1
-        print(i,c)
+    # c = 0 
+    # for i in word:
+    #     gw.set_square_letter(0, c, i)
+    #     c+= 1
+    #     print(i,c)
 
 # Startup code
 
@@ -50,32 +59,11 @@ if __name__ == "__main__":
 # def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
 # def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk))
 
-guess = 'tttet'
-Uguess = guess.lower()
-print(Uguess)
-UcharacterizedGuess = list(Uguess)
-actual = 'tttan'
-Uactual = actual.lower()
-print(Uactual)
-UcharacterizedActual = list(Uactual)
+# guess = 'tttet'
+# Uguess = guess.lower()
+# print(Uguess)
 
-print(UcharacterizedGuess)
-print(UcharacterizedActual)
-row = 1
-col = 0
 color = UNKNOWN_COLOR
-
-
-
-    # if UcharacterizedGuess[i] == UcharacterizedActual[i]:
-    #     prGreen(UcharacterizedGuess[i])
-    #     WordleGWindow.set_square_color(WordleGWindow.get_current_row(), i, CORRECT_COLOR)
-    # elif UcharacterizedGuess[i] in UcharacterizedActual:
-    #     prYellow(UcharacterizedGuess[i])
-    #     WordleGWindow.set_square_color(WordleGWindow.get_current_row(), i, PRESENT_COLOR)
-    # else:
-    #     prLightGray(UcharacterizedGuess[i])
-    #     WordleGWindow.set_square_color(WordleGWindow.get_current_row(), i, MISSING_COLOR)
 
 # CORRECT_COLOR = "#66BB66"       # Light green for correct letters
 # PRESENT_COLOR = "#CCBB66"       # Brownish yellow for misplaced letters
